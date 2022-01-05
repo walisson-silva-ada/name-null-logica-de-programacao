c553654bb9597880cc4f2ec14bfbb1783e0250cb
hugoffrota <hugofrota@gmail.com>
2022-01-05 18:16:23 -0300

    Consertando o exercicio

--- Super_Digito_Function.py
+++ Super_Digito_Function.py
@@ -1,48 +1,68 @@
-def super_digito(n,k):
-    if k == 1:        
-        return(n)
+# def concatena(n,k):
+#     if k == 1:        
+#         return(n)
+#     return (str(n) + ' ' + (concatena( str(n), k-1)))
 
-    return (str(n) + ' ' + (super_digito( str(n), k-1)))
-print(super_digito(9875,4))
-teste1 = super_digito(9875,4)
-type(teste1)
+    
+# print(concatena(9875,4))
+# teste1 = concatena(9875,4)
 
-numero = teste1
-type(numero)
-print(numero)
-numero.replace(" ","")
-print(numero)
-#lista = list(numero)
-#print(lista)
-# nova_lista = []
-# for elemento in lista:
-#     nova_lista.append((int(elemento)))
-#     soma = sum(nova_lista)
-# print(soma)
+# str(9875) * 4 
+# str(n) * k 
+# n = 9875
+# k = 4
+# nova = str(n) + ' '
+# nova*k
 
 
-# print(super_digito(9875,4))
+# def super_digit(n,k):
+#     nova = (str(n) + ' ')*k
+#     while k != 1:
+#         nova = str(n) * k
+#         k -= 1
+#     lista = list(nova.replace(" ",""))
+#     #print(lista)
+#     nova_lista = []
+#     for elemento in lista:
+#         nova_lista.append((int(elemento)))
+#         soma = sum(nova_lista)
+#     if soma > 10:
+#         return super_digit(str(soma))
+#     else:
+#         print(soma)
+#         return (soma)
+        
+#super_digit(n,k)
 
-# testin = '9875'
-# print(list(testin))
-# lista = list(testin)
+# def sum_of_digit( p ):
+#     if p == 0:
+#         return 0
+
+#     return (p % 10 + sum_of_digit(int(p / 10)))
+
+# print(sum_of_digit(p))
+n= str(input('Insira o número '))
+k= int(input('Insira a quantidade de vezes '))
+p = list(n*k)
+print(p)
+def soma_dos_digitos(p):
+    soma = 0
+    if len(p) > 1:
+        for elemento in p:
+            inteiro = int(elemento)
+            soma += inteiro
+            return soma_dos_digitos(soma)
+    print(soma)
+    return(soma)
+soma_dos_digitos(p)
+
+
+# lista = list(p)
 # nova_lista = []
 # for elemento in lista:
 #     nova_lista.append(int(elemento))
-    
-# print(nova_lista)
-# soma = sum(nova_lista)
-# print(soma)
+#     soma = sum(nova_lista)
 
-# def super_digito(n,k):
-#     while k != 0:
-#         #return(n)
-#         return(super_digito(str(n) + ' ' + str(n),k-1))
-#     return(n)
-
-# print(super_digito(9875,4))
-
-# n = str(9875)
-# k = 4
-# p = n*4
-# print(p)
+# for elemento in lista:
+#          nova_lista.append((int(elemento)))
+#          soma = sum(nova_lista)
