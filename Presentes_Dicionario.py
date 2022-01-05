15c82245200c04f85ba423d9fda45d67a7b69da0
hugoffrota <hugofrota@gmail.com>
2022-01-05 12:20:50 -0300

    Corrigindo questão de presentes

--- Presentes_Dicionario.py
+++ Presentes_Dicionario.py
@@ -5,31 +5,50 @@
 # 'jocelina': ('xicara', 'meias', 'perfume'),
 # 'elaine': ('sandalia', 'sapatilha', 'camiseta')
 
-#}
+# }
+
 quantidade = int(input('Insira o número de pessoas '))
 i = 0
 dicionario_presentes = {}
 while i < quantidade:
-    tudao = input('Insira aqui o nome e os 3 presentes da pessoa')
-    valores = tudao.split(' ')
-    chave = valores[0]
-    valores.pop(0)
-    dicionario_presentes[chave] = valores
+    entrada_info = input('Insira aqui o nome e os 3 presentes da pessoa ')
+    valores_separados = entrada_info.split(' ')
+    chave = valores_separados[0]
+    valores_separados.pop(0)
+    dicionario_presentes[chave] = valores_separados
     i += 1
     
 print(dicionario_presentes)
+nome = [] 
+presente = [] 
+numero_verificacores = int(input('Insira quantas pessoas quer verificar o presente. '))
+n = 0 
+while n < numero_verificacores: 
+
+    nome_presente = input('insira o nome da pessoa e o presente que deseja dar ') 
+    separado = nome_presente.split(' ') 
+    nome.append(separado[0]) 
+    presente.append(separado[1]) 
+    n += 1 
+
+
+j = 0
+for j in range(len(nome)):
+    if presente[j] in dicionario_presentes[nome[j]]:
+        print('Uhul! Seu amigo secreto vai adorar o/')
+    else: print('Tente Novamente!')
 
 
 
-nome_presente = input('insira o nome da pessoa e o presente que deseja dar')
-presente = nome_presente.split(' ')
-nome = presente[0]
-presentinho = presente[1]
-
-print(presente)
-
-
-if presentinho in dicionario_presentes[nome]:
-    
-    print('Uhul! Seu amigo secreto vai adorar o/')
-else: print('Tente Novamente!')
+# 5
+# iara mochila estojo lapis
+# adelar sapato camisa carteira
+# jessica agenda bolsa brincos
+# jocelina xicara meias perfume
+# elaine sandalia sapatilha camiseta
+# jessica carteira
+# jessica agenda
+# iara sandalia
+# elaine mochila
+# iara mochila
+# adelar carteira
